7a51d7e309e8ee2748cfe0a56a224ba6d994c9a3
Karpets Oleksandr <karpetsoleksandrschool10@gmail.com>
2024-10-20 11:49:55 +0300

    Fix unknown ShopRequest

---
+++ BankProject/API/Bank.API/DTOs/ShopRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Bank.API.DTOs
+{
+    public class ShopRequest
+    {
+        public string FromAccount { get; set; }
+
+        public string ToAccount { get; set; }
+
+        public string Amount { get; set; }
+
+        public string Email { get; set; }
+
+        public Guid OrderId { get; set; }
+    }
+}
